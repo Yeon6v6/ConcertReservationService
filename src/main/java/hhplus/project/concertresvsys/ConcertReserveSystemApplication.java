92006a37c1e5aa25fa8b812c2b76c06838eb4d4c
yeon <merleawe012@gmail.com>
2025-01-01 13:45:49 +0900

    init project

---
+++ src/main/java/hhplus/project/concertresvsys/ConcertReserveSystemApplication.java
@@ -0,0 +1,13 @@
+package hhplus.project.concertresvsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertReserveSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcertReserveSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hhplus/project/concertresvsys/ConcertReserveSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package hhplus.project.concertresvsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertReserveSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
