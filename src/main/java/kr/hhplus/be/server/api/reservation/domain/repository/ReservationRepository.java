43cc8764042ed5223db3c2d3334211a1006c2eaf
yeon <merleawe012@gmail.com>
2025-01-16 23:12:36 +0900

    feat : 좌석 예약 동시성 체크 및 테스트 코드 추가 / 테스트 코드 sql 설정 값 추가

--- src/main/java/kr/hhplus/be/server/api/reservation/domain/repository/ReservationRepository.java
+++ src/main/java/kr/hhplus/be/server/api/reservation/domain/repository/ReservationRepository.java
@@ -1,9 +1,13 @@
 package kr.hhplus.be.server.api.reservation.domain.repository;
 
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import kr.hhplus.be.server.api.common.type.ReservationStatus;
 import kr.hhplus.be.server.api.reservation.domain.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -16,10 +20,12 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
     // 특정 콘서트와 날짜에 해당하는 예약 리스트 조회
     List<Reservation> findByConcertIdAndScheduleDate(Long concertId, LocalDate scheduleDate);
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints({@QueryHint(name = "jakarta.persistence.lock.timeout", value = "3000")}) //동시성이 많을 수 있으므로 락 대기시간 설정
+    @Query("SELECT r FROM Reservation r WHERE r.seatId = :seatId")
     // 특정 좌석에 대한 예약 조회
-    Reservation findBySeatId(@Param("seatId") Long seatId);
+    Reservation findBySeatIdWithLock(@Param("seatId") Long seatId);
 
-    // 좌석에 대한 중복 예약 확인
-    boolean existsBySeatId(Long seatId);
-
+    @Query("SELECT r FROM Reservation r WHERE r.seatId = :seatId")
+    List<Reservation> findBySeatId(Long seatId);
 }
