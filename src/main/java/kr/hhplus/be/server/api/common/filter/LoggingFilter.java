a9e91cd33421992a03e5484f81311f6b6854e1bd
yeon <merleawe012@gmail.com>
2025-01-16 19:02:32 +0900

    feat : 로그 설정 값 추가

---
+++ src/main/java/kr/hhplus/be/server/api/common/filter/LoggingFilter.java
@@ -0,0 +1,35 @@
+package kr.hhplus.be.server.api.common.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+@Slf4j
+@WebFilter(urlPatterns = "/*")
+public class LoggingFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("[LoggingFilter] Initialized");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        long startTime = System.currentTimeMillis();
+        log.info("[LoggingFilter] 요청 IP : {}", request.getRemoteAddr());
+        log.info("[LoggingFilter] 요청 시간 : {}", System.currentTimeMillis());
+
+        chain.doFilter(request, response);
+
+        long duration = System.currentTimeMillis() - startTime;
+        log.info("[LoggingFilter] 요청 처리 시간 : {} ms", duration);
+    }
+
+    @Override
+    public void destroy() {
+        log.info("[LoggingFilter] 종료 시간 : {}", System.currentTimeMillis());
+    }
+}
